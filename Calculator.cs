75fd00ca45bc96e048f3a659e36ac27ddac2aa64
Janis Simansons <valisjs@inbox.lv>
2020-05-16 20:11:28 +0300

    version 20:11

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCalculator();
+        }
+
+        private void InitializeCalculator()
+        {
+            this.BackColor = Color.Purple;
+
+            string buttonName = null;
+            Button button = null;
+
+            for(int i = 0; i < 10; i++)
+            {
+                buttonName = "button" + i;
+                button = (Button)this.Controls[buttonName];
+                button.Text = i.ToString();             
+            }
+
+            button0.Text = "0";
+
+        }
+
+    }
+}
